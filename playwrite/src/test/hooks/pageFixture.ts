93c0d8aa241e8458d6d75b7ed40e7426c4d90327
ErichGiusseppe <eg.soto@uniandes.edu.co>
2023-05-14 00:59:50 -0500

    v3_42

--- playwrite/src/test/hooks/pageFixture.ts
+++ playwrite/src/test/hooks/pageFixture.ts
@@ -1,12 +1,18 @@
 import { Page } from "@playwright/test";
-import { LoginPage } from "../pages/loginPage";
-import { PrincipalPage } from "../pages/principalPage";
+import { LoginPage, LoginPage3_42 } from "../pages/loginPage";
+import { PrincipalPage, PrincipalPage3_42 } from "../pages/principalPage";
 export const pageFixture = {
     //@ts-ignore
     page: undefined as Page,
     //@ts-ignore
+    page2:undefined as Page,
+    //@ts-ignore
     login_page:undefined as LoginPage,
     //@ts-ignore
     principal_page:undefined as PrincipalPage,
+    //@ts-ignore
+    login_page_3_42:undefined as LoginPage3_42,
+    //@ts-ignore
+    principal_page_3_42:undefined as PrincipalPage3_42
     
 }
--- playwrite/src/test/versions/3.42/hooks/hooks.ts
+++ playwrite/src/test/versions/3.42/hooks/hooks.ts
@@ -3,12 +3,11 @@ import { pageFixture } from "./pageFixture";
 import { Browser, BrowserContext, Page, chromium } from "@playwright/test";
 import { LoginPage } from "../pages/loginPage";
 import { PrincipalPage } from "../pages/principalPage";
-import { PrincipalPage3_42 } from "../pages/principalPage";
-import { LoginPage3_42 } from "../pages/loginPage";
+
 let browser: Browser;
 let page: Page;
+
 let context: BrowserContext;
-let page2:Page;
 BeforeAll(async function(){
     browser = await chromium.launch({headless:false}); //si quisieramos el chromium headless {headless:true}
 });
@@ -16,13 +15,9 @@ Before(async function(){
     
     context = await browser.newContext();
     page = await context.newPage();
-    page2= await context.newPage();
     pageFixture.page=page;
-    pageFixture.page2=page2;
     pageFixture.login_page=new LoginPage(page)
     pageFixture.principal_page=new PrincipalPage(page)
-    pageFixture.login_page_3_42=new LoginPage3_42(page)
-    pageFixture.principal_page_3_42=new PrincipalPage3_42(page)
 
 });
 After(async function ({pickle}){
