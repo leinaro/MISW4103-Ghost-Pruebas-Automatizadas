3e40c0938296dde46778b9988504f5dc85bdbd06
Ines Rojas <ia.rojas2@uniandes.edu.co>
2023-05-14 12:27:27 -0500

    screenshots report

--- reporte/index.js
+++ reporte/index.js
@@ -77,39 +77,27 @@ async function run() {
 }
 (async () => console.log(await run()))();
 
-function step(f, s, info) {
-  filterInfo = info[`${f}-${s}`]
-  console.log(info)
+function createReport(resInfo) {
+  let features = fs.readdirSync(v3Directory).filter(function(x) {
+    return x !== '.DS_Store' && x !== 'ignore-other-file.js';
+  });
 
-  let diffHtml = `
-    <div class="row">
-      <div class="col-xs-12 col-md-6">
-        <img class="img-fluid" src="./${f}/compare-${s}.png" id="diffImage" label="Diff">
-      </div>
-    </div>`;
-  if (filterInfo.misMatchPercentage < 5) {
-    
-  } else {
-    
-  }
   return `
-  <h2>Step: ${s}</h2>
-  <p><strong>RawMisMatchPercentage: </strong> ${filterInfo.rawMisMatchPercentage}</p>
-  <p><strong>RisMatchPercentage: </strong> ${filterInfo.misMatchPercentage}</p>
-  <p><strong></strong></p>
-  <p>Data: ${JSON.stringify(filterInfo)}</p>
-  <div class="row">
-    <div class="col-xs-12 col-md-5 m-2">
-      <span class="imgname">Ghost 3.41</span>
-      <img class="img-fluid" src="./${f}/v3-${s}.png" id="refImage" label="Reference">
-    </div>
-    <div class="col-xs-12 col-md-5 m-2">
-      <span class="imgname">Ghost 4.44</span>
-      <img class="img-fluid" src="./${f}/v4-${s}.png" id="testImage" label="Test">
-    </div>
-  </div>
-  ${diffHtml}
-</div>`
+    <html>
+      <head>
+        <title> Reporte </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+      </head>
+      <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <h1>Reporte Semana 6</h1>
+        <div class="container-fluid">
+          <div class="accordion" id="accordionExample">
+            ${features.map(b => feature(b, resInfo))}
+          </div>
+        </div>
+      </body>
+    </html>`
 }
 
 function feature(f, info) {
@@ -120,35 +108,79 @@ function feature(f, info) {
   console.log("info "+info)
   screenshots = screenshots.sort(sorter);
 
-  return `<div class="row">
-      <div class="browser col-xs-12 col-md-5">
-      <h2>Feature: ${f}</h2>
+  return `
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="heading${f}">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${f}" aria-expanded="true" aria-controls="collapse${f}">
+        Feature: ${f}
+      </button>
+    </h2>
+    <div id="collapse${f}" class="accordion-collapse collapse" aria-labelledby="heading${f}" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        ${
+          screenshots.reduce((accumulator, s) => accumulator + step(f, s, info), "")
+        }
       </div>
-      ${screenshots.map(s => step(f, s, info))}
     </div>
-    `
+  </div>`
 }
 
-function createReport(resInfo) {
-  let features = fs.readdirSync(v3Directory).filter(function(x) {
-    return x !== '.DS_Store' && x !== 'ignore-other-file.js';
-  });
+function step(f, s, info) {
+  console.log(info)
+
+  filterInfo = info[`${f}-${s}`]
 
   return `
-    <html>
-    <head>
-    <title> Reporte </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    </head>
-    <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <h1>Reporte Semana 6</h1>
-    <div class="container-fluid m-5">
-    
-    ${features.map(b => feature(b, resInfo))}
+  <div class="accordion" id="accordionStep">
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="heading${s}">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${s}" aria-expanded="true" aria-controls="collapse${s}">
+          Step: ${s}
+        </button>
+      </h2>
+      <div id="collapse${s}" class="accordion-collapse collapse" aria-labelledby="heading${s}" data-bs-parent="#accordionStep">
+        <div class="accordion-body">
+          <table class="table">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">RawMisMatchPercentage</th>
+                <th scope="col">RisMatchPercentage</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>${filterInfo.rawMisMatchPercentage}</td>
+                <td>${filterInfo.misMatchPercentage}%</td>
+              </tr>
+            </tbody>
+          </table>
+          <h3>Comparativa</h3>
+          <table class="table-responsive table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Ghost 3.41</th>
+                <th scope="col">Ghost 4.44</th>
+                <th scope="col">Resultado</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <img class="img-fluid" src="./${f}/v3-${s}.png" id="refImage" label="Reference">
+                </td>
+                <td>
+                  <img class="img-fluid" src="./${f}/v4-${s}.png" id="testImage" label="Test">
+                </td>
+                <td>
+                  <img class="img-fluid" src="./${f}/compare-${s}.png" id="diffImage" label="Diff">
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-    </body>
-    </html>`
+  </div>`
 }
 
 function sorter(a, b) {
